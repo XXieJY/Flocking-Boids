30de15a8ab7e1eea1c3f9ad22c77d553264b4807
John Wakefield <john.architect@gmail.com>
2016-10-18 18:24:41 +0100

    Add files via upload

---
+++ Swarm.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2016 robosoup
+// www.robosoup.com
+
+using System.Collections.Generic;
+
+namespace Boids
+{
+    class Swarm
+    {
+        public List<Boid> Boids = new List<Boid>();
+
+        public Swarm(int boundary)
+        {
+            for (var i = 0; i < 15; i++)
+                Boids.Add(new Boid((i > 12), boundary));
+        }
+
+        public void MoveBoids()
+        {
+            foreach (var boid in Boids)
+                boid.Move(Boids);
+        }
+    }
+}
